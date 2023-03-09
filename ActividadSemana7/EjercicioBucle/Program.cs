f4f1bd6694bbed3c6651c8f1f5179ef4b0c83b52
oneyda21 <124007400+oneyda21@users.noreply.github.com>
2023-03-09 15:12:58 -0600

    Ejercicio

---
+++ ActividadSemana7/EjercicioBucle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("Introduzca un número entero: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
